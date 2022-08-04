03a4e5b3919f180946d7963f03ac78fa4c713c41
Artem <timoseyka@gmail.com>
2022-08-04 09:19:30 +0300

    add Load User Profile Image func

--- TwitterClone/Utils/User.swift
+++ TwitterClone/Utils/User.swift
@@ -11,15 +11,19 @@ struct User {
     let fullname: String
     let email: String
     let username: String
-    let profileImageUrl: String
+    var profileImageUrl: URL?
     let uid: String
     
     init(uid: String, dictionary: [String: AnyObject]) {
         self.uid = uid
         
         self.username = dictionary["username"] as? String ?? ""
-        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
         self.email = dictionary["email"] as? String ?? ""
         self.fullname = dictionary["fullname"] as? String ?? ""
+        
+        if let profileImageUrlString = dictionary["profileImageUrl"] as? String {
+            guard let url = URL(string: profileImageUrlString) else { return }
+            self.profileImageUrl = url
+        }
     }
 }
